7e30b867f7dc5637fac7d8a7f3e7e2c125c32316
root <toor>
2021-11-15 11:23:27 +0200

    Initial commit

---
+++ sf-pet-clinic/src/test/java/com/rpr/sf/sfpetclinic/SfPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpr.sf.sfpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
